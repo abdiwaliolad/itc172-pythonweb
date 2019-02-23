119d4ad3619b36e64ce96e73699dd540b221eda6
abdiwaliolad <abdiwaliolad@gmail.com>
2019-02-22 20:22:38 -0800

    resource
    add resource file

--- pythonclub/club/urls.py
+++ pythonclub/club/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
 path('', views.index, name='index'),
 
-#path('getMeeting', views.getmeeting, name='Meeting'),
+path('getmeeting/', views.getmeeting, name='meeting'),
+path('resources/', views.resources, name='resources'),
+path('meetingDetail/<int:id>', views.meetingDetail, name='meetingDetail'),
  
 ]
--- pythonclub/club/views.py
+++ pythonclub/club/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render, get_list_or_404
-from django.http import HttpResponse
-from.models import Meeting
+from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Meeting,Resource
 
 # Create your views here.
 def index(request):
     return render(request, 'club/index.html')
 
-#def getmeeting(request):
-    #meeting_list=Meeting.objects.all()
-    #return render(request, 'club/meeting.html', {'meeting_list': meeting_list} )
+def getmeeting(request):
+    meeting_list= Meeting.objects.all()
+    return render(request, 'club/meeting.html', {'meeting_list': meeting_list})
+
+def resources(request):
+    resource_list= Resource.objects.all()
+    return render(request,'club/resources.html',{'resource_list': resource_list})
+
+def meetingDetail(request, id):
+    meeting= get_object_or_404(Meeting, pk=id) 
+    return render(request, 'club/meetingdetail.html',{'meeting': meeting})   
+
 
